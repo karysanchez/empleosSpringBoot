4268d0a2151a12882c78c3f8627eecb2c2f94c21
“Karina.Sanchez” <karoty_25@hotmail.com>
2023-04-05 23:05:16 -0500

    thymeleaf manejo de services paquete

---
+++ src/main/java/com/karosanpu/service/IVacantesService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.karosanpu.service;
+
+import java.util.List;
+
+import com.karosanpu.model.Vacante;
+
+/**
+ * @author KaRoT
+ *
+ */
+public interface IVacantesService {
+	List<Vacante> buscarTodas();
+}
---
+++ src/main/java/com/karosanpu/service/VacantesServiceImpl.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.karosanpu.service;
+
+import java.util.List;
+
+import com.karosanpu.model.Vacante;
+
+/**
+ * @author KaRoT
+ *
+ */
+public class VacantesServiceImpl implements IVacantesService {
+
+	@Override
+	public List<Vacante> buscarTodas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
