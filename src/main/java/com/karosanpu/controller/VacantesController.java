cd9065cfca2f9420b13cc92f8523eaebf57af849
Karina.Sanchez <karoty_25@hotmail.com>
2023-04-21 17:08:41 -0500

    formulario vacantes

--- src/main/java/com/karosanpu/controller/VacantesController.java
+++ src/main/java/com/karosanpu/controller/VacantesController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -25,6 +26,39 @@ public class VacantesController {
 	@Autowired
 	private IVacantesService serviceVacantes;
 
+	/**
+	 * Método para cargar el formulario de registro de vacantes
+	 * 
+	 * @return
+	 */
+	@GetMapping("/create")
+	public String crear() {
+		return "vacantes/formVacante";
+	}
+
+	/**
+	 * Método para guardar lo ingresado en el formulario (por el momento solo
+	 * retornara a una vista list vacantes)
+	 * 
+	 * @return
+	 */
+	@PostMapping("/save")
+	public String guardar(@RequestParam("nombre") String nombre, @RequestParam("descripcion") String descripcion,
+			@RequestParam("estatus") String estatus, @RequestParam("fecha") String fecha,
+			@RequestParam("destacado") int destacado, @RequestParam("salario") double salario,
+			@RequestParam("detalles") String detalles) {
+		
+		System.out.println("Nombre Vacante: " + nombre);
+		System.out.println("Descripcion: " + descripcion);
+		System.out.println("Estatus: " + estatus);
+		System.out.println("Fecha Publicación: " + fecha);
+		System.out.println("Destacado: " + destacado);
+		System.out.println("Salario Ofrecido: " + salario);
+		System.out.println("detalles: " + detalles);
+		
+		return "vacantes/listVacantes";
+	}
+
 	@GetMapping("/delete")
 	public String eliminar(@RequestParam("id") int idVacante, Model model) {
 		System.out.println("Borrando vacante con id: " + idVacante);
@@ -34,7 +68,7 @@ public class VacantesController {
 
 	@GetMapping("/view/{id}")
 	public String verDetalle(@PathVariable("id") int idVacante, Model model) {
-		
+
 		Vacante vacante = serviceVacantes.buscarPorId(idVacante);
 
 		System.out.println("Vacante: " + vacante);
