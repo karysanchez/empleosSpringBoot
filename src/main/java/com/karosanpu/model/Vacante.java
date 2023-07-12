6626809ed5b54d88f48bd9f99e6789eda9b351bd
“Karina.Sanchez” <karoty_25@hotmail.com>
2023-07-11 22:10:09 -0500

    catalgo dinamico relaciones

--- src/main/java/com/karosanpu/model/Vacante.java
+++ src/main/java/com/karosanpu/model/Vacante.java
@@ -17,132 +17,156 @@ public class Vacante {
 	private Date fecha;
 	private Double salario;
 	private Integer destacado;
-	private String imagen="no-image.png";
+	private String imagen = "no-image.png";
 	private String estatus;
 	private String detalles;
-	
+	private Categoria categoria;
+
 	/**
 	 * @return the id
 	 */
 	public Integer getId() {
 		return id;
 	}
+
 	/**
 	 * @param id the id to set
 	 */
 	public void setId(Integer id) {
 		this.id = id;
 	}
+
 	/**
 	 * @return the nombre
 	 */
 	public String getNombre() {
 		return nombre;
 	}
+
 	/**
 	 * @param nombre the nombre to set
 	 */
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+
 	/**
 	 * @return the descripcion
 	 */
 	public String getDescripcion() {
 		return descripcion;
 	}
+
 	/**
 	 * @param descripcion the descripcion to set
 	 */
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
+
 	/**
 	 * @return the fecha
 	 */
 	public Date getFecha() {
 		return fecha;
 	}
+
 	/**
 	 * @param fecha the fecha to set
 	 */
 	public void setFecha(Date fecha) {
 		this.fecha = fecha;
 	}
-	
+
 	/**
 	 * @return the salario
 	 */
 	public Double getSalario() {
 		return salario;
 	}
+
 	/**
 	 * @param salario the salario to set
 	 */
 	public void setSalario(Double salario) {
 		this.salario = salario;
 	}
-	
+
 	/**
 	 * @return the destacado
 	 */
 	public Integer getDestacado() {
 		return destacado;
 	}
+
 	/**
 	 * @param destacado the destacado to set
 	 */
 	public void setDestacado(Integer destacado) {
 		this.destacado = destacado;
 	}
-	
+
 	/**
 	 * @return the imagen
 	 */
 	public String getImagen() {
 		return imagen;
 	}
-	
+
 	/**
 	 * @param imagen the imagen to set
 	 */
 	public void setImagen(String imagen) {
 		this.imagen = imagen;
 	}
-	
+
 	/**
 	 * @return the estatus
 	 */
 	public String getEstatus() {
 		return estatus;
 	}
-	
+
 	/**
 	 * @param estatus the estatus to set
 	 */
 	public void setEstatus(String estatus) {
 		this.estatus = estatus;
 	}
-	
+
 	/**
 	 * @return the detalles
 	 */
 	public String getDetalles() {
 		return detalles;
 	}
-	
+
 	/**
 	 * @param detalles the detalles to set
 	 */
 	public void setDetalles(String detalles) {
 		this.detalles = detalles;
 	}
-	
+
+	/**
+	 * @return the categoria
+	 */
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
 	@Override
 	public String toString() {
 		return "Vacante [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", fecha=" + fecha
 				+ ", salario=" + salario + ", destacado=" + destacado + ", imagen=" + imagen + ", estatus=" + estatus
-				+ ", detalles=" + detalles + "]";
+				+ ", detalles=" + detalles + ", categoria=" + categoria + "]";
 	}
-	
+
 }
